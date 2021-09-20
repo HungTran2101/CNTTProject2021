0072a7602ef987a8524991ae8668465a04f1c388
HungTran2101 <hung9cong@gmail.com>
2021-09-20 13:16:36 +0700

    add jump mechanic

--- Wolf Marathon/obsticals.js
+++ Wolf Marathon/obsticals.js
@@ -1,5 +1,10 @@
 class obsticals{
-    constructor(){
-        
+    constructor(x, y){
+        this.x = x - 200;
+        this.y = y;
+        this.img = new Image;
+    }
+    spawn(){
+        let x = Math.round(Math.random() * 100);
     }
 }
